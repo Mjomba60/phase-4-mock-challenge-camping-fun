411c020bee79dba74888e3611b3cfa328a9726ba
George Samuel <geosam2708@gmail.com>
2023-06-29 19:38:36 +0300

    Added response for not found

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,5 +1,13 @@
 class CampersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+
     def index
         render json: Camper.all, status: :ok
+    end
+
+    private
+
+    def render_record_not_found_response
+        render json: {error: "Camper not found"}, status: :not_found
     end
 end
